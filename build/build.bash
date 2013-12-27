fea3514adb9b175ec2c10ec2c39446f773f7a7a7
darky83 <arno@troublenow.org>
2013-12-27 15:05:31 +0100

    big push of new files

--- build/build.bash
+++ build/build.bash
@@ -21,7 +21,9 @@
 # +---------------------------------------------------+
 # Variables
 # +---------------------------------------------------+
-
+version="3.0.0.0 beta"
+logdir="/var/log/EFA"
+gitdlurl="https://raw.github.com/E-F-A/v3/master/build"
 # +---------------------------------------------------+
 
 # +---------------------------------------------------+
@@ -29,6 +31,7 @@
 # +---------------------------------------------------+
 func_upgradeOS () {
     yum -y upgrade
+    rpm -e wireless-tools
 }
 # +---------------------------------------------------+
 
@@ -46,6 +49,15 @@ func_repoforge () {
 # +---------------------------------------------------+
 func_clamav () {
     yum -y install clamav
+    freshclam
+}
+# +---------------------------------------------------+
+
+# +---------------------------------------------------+
+# configure apache
+# +---------------------------------------------------+
+func_apache () {
+    echo "apache configuration"
 }
 # +---------------------------------------------------+
 
@@ -62,10 +74,115 @@ func_kernmodules () {
 # +---------------------------------------------------+
 
 # +---------------------------------------------------+
+# enable and disable services
+# +---------------------------------------------------+
+func_services () {
+    chkconfig ip6tables off
+    chkconfig cpuspeed off
+    chkconfig lvm2-monitor off
+    chkconfig mdmonitor off
+    chkconfig netfs off
+    # disable postfix for now and enable after efa-init
+    chkconfig postfix off 
+    chkconfig smartd off
+}
+# +---------------------------------------------------+
+
+# +---------------------------------------------------+
+# EFA specific customization 
+# +---------------------------------------------------+
+func_efarequirements () {
+    # Write version file
+    echo "EFA-$version" > /etc/EFA-Version
+
+    # create EFA config file
+    touch /etc/EFA-Config
+    
+    # pre-create the EFA update directory
+    mkdir -p /var/EFA/update
+    
+    # write issue file
+    echo "" > /etc/issue
+    echo "------------------------------" >> /etc/issue
+    echo "--- Welcome to EFA $version ---" >> /etc/issue
+    echo "------------------------------" >> /etc/issue
+    echo "  http://www.efa-project.org  " >> /etc/issue
+    echo "------------------------------" >> /etc/issue
+    echo "" >> /etc/issue
+    echo "First time login: root/EfaPr0j3ct" >> /etc/issue
+    
+    # Grab EFA specific scripts/programs
+    /usr/bin/wget -q -O /usr/local/sbin/EFA-Init -o /var/log/efa/wget.log $gitdlurl/EFA/EFA-Init
+    chmod 700 /usr/local/sbin/EFA-Init
+    /usr/bin/wget -q -O /usr/local/sbin/EFA-Configure -o /var/log/efa/wget.log $gitdlurl/EFA/EFA-Configure
+    chmod 700 /usr/local/sbin/EFA-Configure
+    /usr/bin/wget -q -O /usr/local/sbin/EFA-Update -o /var/log/efa/wget.log $gitdlurl/EFA/EFA-Update
+    chmod 700 /usr/local/sbin/EFA-Update
+    /usr/bin/wget -q -O /usr/local/sbin/EFA-SA-Update -o /var/log/efa/wget.log $gitdlurl/EFA/EFA-SA-Update
+    chmod 700 /usr/local/sbin/EFA-SA-Update
+    
+    # Write SSH banner
+  sed -i "/^#Banner / c\#Banner" /etc/ssh/sshd_config
+  cat > /etc/banner << 'EOF'
+       Welcome to E.F.A. (http://www.efa-project.org)
+
+ Warning!
+
+ THIS IS A PRIVATE COMPUTER SYSTEM. It is for authorized use only.
+ Users (authorized or unauthorized) have no explicit or implicit
+ expectation of privacy.
+
+ Any or all uses of this system and all files on this system may
+ be intercepted, monitored, recorded, copied, audited, inspected,
+ and disclosed to authorized site and law enforcement personnel,
+ as well as authorized officials of other agencies, both domestic
+ and foreign.  By using this system, the user consents to such
+ interception, monitoring, recording, copying, auditing, inspection,
+ and disclosure at the discretion of authorized site personnel.
+
+ Unauthorized or improper use of this system may result in
+ administrative disciplinary action and civil and criminal penalties.
+ By continuing to use this system you indicate your awareness of and
+ consent to these terms and conditions of use.   LOG OFF IMMEDIATELY
+ if you do not agree to the conditions stated in this warning.  
+EOF
+}
+# +---------------------------------------------------+
+
+# +---------------------------------------------------+
+# Cron settings
+# +---------------------------------------------------+
+func_cron () {
+    /usr/bin/wget -q -O /etc/cron.daily/EFA-Daily-cron -o /var/log/efa/wget.log $gitdlurl/EFA/EFA-Daily-cron
+    chmod 700 /etc/cron.daily/EFA-Daily-cron
+    /usr/bin/wget -q -O /etc/cron.monthly/EFA-Monthly-cron -o /var/log/efa/wget.log $gitdlurl/EFA/EFA-Monthly-cron
+    chmod 700 /etc/cron.monthly/EFA-Monthly-cron
+}
+# +---------------------------------------------------+
+
+# +---------------------------------------------------+
+# Clean-up
+# +---------------------------------------------------+
+func_cleanup () {
+    
+    # DISABLED FOR NOW UNTIL TESTING IS OVER...
+    # Clean SSH keys (generate at first boot)
+    #/bin/rm /etc/ssh/ssh_host_*
+    
+    # Secure SSH
+    #sed -i '/^#PermitRootLogin/ c\PermitRootLogin no' /etc/ssh/sshd_config
+}
+
+# +---------------------------------------------------+
 # Main logic (this is where we start calling out functions)
 # +---------------------------------------------------+
 func_upgradeOS
 func_repoforge
 func_clamav
+func_apache
 func_kernmodules
+func_services
+func_efarequirements
+func_cron
+func_cleanup
 # +---------------------------------------------------+
