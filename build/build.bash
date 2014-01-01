5af4cb787d7f62c09e5bf0d18a8b85e4c63c6967
darky83 <arno@troublenow.org>
2014-01-01 19:45:17 +0100

    Fix mailgraph se-linux

--- build/build.bash
+++ build/build.bash
@@ -861,6 +861,7 @@ func_cleanup () {
     
     # Fix SE-Linux security issues
     restorecon -r /var/www
+    chcon -v --type=httpd_sys_content_t /var/lib/mailgraph*
     
     # zero disks for better compression (when creating VM images)
     # this can take a while so disabled for now until we start creating images.
