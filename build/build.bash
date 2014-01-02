2dc98643cedb482a47b1df5f2a2f201edd127d19
darky83 <arno@troublenow.org>
2014-01-02 21:49:42 +0100

    just rembered I already knew this one.. (time for bed)

--- build/build.bash
+++ build/build.bash
@@ -34,14 +34,6 @@
 # http://pralab.diee.unica.it/en/BayesOCR
 # http://pralab.diee.unica.it/imageCerberus
 
-# Issue:
-# Mailscanner won't run correctly, when debugging it mentions:
-#
-# 20:35:02 Insecure dependency in open while running with -T switch at /usr/lib64/perl5/IO/File.pm line 185.
-#
-# Looks like some sort of dependency error but can't figure out what is causing this.
-
-
 # +---------------------------------------------------+
 # Variables
 # +---------------------------------------------------+
@@ -281,6 +273,9 @@ func_mailscanner () {
     mkdir /var/spool/MailScanner/incoming
     echo "none /var/spool/MailScanner/incoming tmpfs defaults 0 0">>/etc/fstab
     mount -a
+    
+    # Fix (workaround) the "Insecure dependency in open while running with -T switch at /usr/lib64/perl5/IO/File.pm line 185" error
+    sed -i '/^#!\/usr\/bin\/perl -I\/usr\/lib\/MailScanner/ c\#!\/usr\/bin\/perl -I\/usr\/lib\/MailScanner\ -U' /usr/sbin/MailScanner
 }
 # +---------------------------------------------------+
 
