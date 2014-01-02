8487436551601a5ab1de1a5fcc0372217943de84
darky83 <arno@troublenow.org>
2014-01-02 15:41:43 +0100

    fix the  warning: inet_protocols: error message
    Because we disabled ipv6 (later release we will probably add this but I
    have no way of testing currently)

--- build/build.bash
+++ build/build.bash
@@ -132,6 +132,7 @@ func_mysql () {
 func_postfix () {
     mkdir /etc/postfix/ssl
     echo /^Received:/ HOLD>>/etc/postfix/header_checks
+    postconf -e "inet_protocols = ipv4"
     postconf -e "inet_interfaces = all"
     postconf -e "mynetworks = 127.0.0.0/8"
     postconf -e "header_checks = regexp:/etc/postfix/header_checks"
