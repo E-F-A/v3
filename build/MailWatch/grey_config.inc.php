3fb0686e5a584381eb9e9ae0b640c3a7864eff28
Shawn Iverson <shawniverson@gmail.com>
2014-01-06 20:05:29 -0500

    remove greylist files for mailwatch

--- build/MailWatch/grey_config.inc.php
+++
@@ -1,8 +0,0 @@
-<?
-$db_db   = "sqlgrey";
-$db_hostname = "localhost";
-$db_user = "sqlgrey";
-$db_pass = "grey";
-
-$db_type = "mysql"; # mysql or pg (pg=postgress)
-?>
--- build/MailWatch/grey_copyright.inc.php
+++
@@ -1,3 +0,0 @@
-<!----
-Based on code adapted from SQLGrey webinterface v0.6 by folkert@vanheusden.com (http://www.vanheusden.com/sgwi/)
----->
