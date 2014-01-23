b61aaba84884a6a7a55990108ed6621a033d61f5
darky83 <arno@troublenow.org>
2014-01-23 14:48:52 +0100

    change version to 3.0.0.0

--- build/build.bash
+++ build/build.bash
@@ -21,7 +21,7 @@
 # +---------------------------------------------------+
 # Variables
 # +---------------------------------------------------+
-version="3.0.0.0-beta"
+version="3.0.0.0"
 logdir="/var/log/EFA"
 gitdlurl="https://raw.github.com/E-F-A/v3/master/build"
 password="EfaPr0j3ct"
