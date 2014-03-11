e30d8d0266efe19f4cf09a44078b0c9cb19bf0e0
Shawn Iverson <shawniverson@gmail.com>
2014-03-10 20:59:30 -0400

    Advance to 3.0.0.5-devel

--- build/build.bash
+++ build/build.bash
@@ -21,12 +21,12 @@
 # +---------------------------------------------------+
 # Variables
 # +---------------------------------------------------+
-version="3.0.0.4-devel"
+version="3.0.0.5-devel"
 logdir="/var/log/EFA"
 gitdlurl="https://raw.github.com/E-F-A/v3/master/build"
 password="EfaPr0j3ct"
 mirror="http://dl.efa-project.org"
-mirrorpath="/build/3.0.0.4"
+mirrorpath="/build/3.0.0.5"
 # +---------------------------------------------------+
 
 # +---------------------------------------------------+
