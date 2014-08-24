7a2bd9e92982f9faa9fe4ab87ea98790cc0c9202
shawniverson <shawniverson@gmail.com>
2014-08-24 10:04:56 -0400

    Prep 3.0.0.5 Branch

--- build/build.bash
+++ build/build.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # +--------------------------------------------------------------------+
-# EFA 3.0.0.5 build script version 20140307
+# EFA 3.0.0.5 build script version 20140824
 # +--------------------------------------------------------------------+
 # Copyright (C) 2013  http://www.efa-project.org
 #
@@ -21,9 +21,9 @@
 # +---------------------------------------------------+
 # Variables
 # +---------------------------------------------------+
-version="3.0.0.5-devel"
+version="3.0.0.5"
 logdir="/var/log/EFA"
-gitdlurl="https://raw.github.com/E-F-A/v3/master/build"
+gitdlurl="https://raw.github.com/E-F-A/v3/3.0.0.5/build"
 password="EfaPr0j3ct"
 mirror="http://dl.efa-project.org"
 mirrorpath="/build/3.0.0.5"
