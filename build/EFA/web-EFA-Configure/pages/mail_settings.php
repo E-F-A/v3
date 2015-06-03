97669238baa6770eb549e76a0b146fe1c93ce419
darky83 <arno@troublenow.org>
2015-06-03 19:13:53 +0200

    Greylisting status

--- build/EFA/web-EFA-Configure/pages/mail_settings.php
+++ build/EFA/web-EFA-Configure/pages/mail_settings.php
@@ -13,14 +13,17 @@ include '../inc/sidebar.php';
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
-            <div class="row">
+            
+            
+            
+<!--            <div class="row">
                 <form role="form-Transport">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Transport
                         </div>
-                        <!-- /.panel-heading -->
+                         /.panel-heading 
                         <div class="panel-body">
                             <div class="table-responsive">
                                 <table class="table table-hover">
@@ -55,16 +58,16 @@ include '../inc/sidebar.php';
                                 </table>
                                 <button type="submit" class="btn btn-default">Add row</button>
                             </div>
-                            <!-- /.table-responsive -->
+                             /.table-responsive 
                         </div>
-                        <!-- /.panel-body -->
+                         /.panel-body 
                     </div>
-                    <!-- /.panel -->
+                     /.panel 
                 </form>
                 </div>
-                <!-- /.col-lg-6 -->
+                 /.col-lg-6 
             </div>
-            <!-- /.row -->
+             /.row -->
 
             <!-- greylisting.row -->
             <div class="row">
@@ -82,8 +85,16 @@ include '../inc/sidebar.php';
                             <p class="text-warning">
                             This however causes an delay in receiving mail, by default this system is configured to reject any email for 5 minutes.
                             </p>
-                            <p class="text-success">Greylisting is currently ENABLED</p>
-                            <button type="submit" class="btn btn-default">Disable</button>
+                            <?php 
+                                define('LIBDIR', "/var/www/html/Admin/EFA/v3/build/EFA/lib-EFA-Configure/new/");
+                                if (exec("sudo $LIBDIR/EFA-func_greylisting.bash --status") == ENABLED) {
+                                    print '<p class="text-success">Greylisting is currently ENABLED</p>';
+                                    print '<button type="submit" class="btn btn-default">Disable</button>';
+                                } else {
+                                    print '<p class="text-danger">Greylisting is currently DISABLED</p>';
+                                    print '<button type="submit" class="btn btn-default">Enable</button>';
+                                }
+                            ?>    
                         </div>
                         <!-- /.panel-body -->
                     </div>
@@ -94,7 +105,7 @@ include '../inc/sidebar.php';
             </div>
             <!-- /greylisting.row -->
 
-            <!-- Outbound mail relay.row -->
+<!--             Outbound mail relay.row 
             <div class="row">
                 <form role="form-obmailrelay">
                 <div class="col-lg-12">
@@ -102,7 +113,7 @@ include '../inc/sidebar.php';
                         <div class="panel-heading">
                             Outbound mail relay
                         </div>
-                        <!-- /.panel-heading -->
+                         /.panel-heading 
                         <div class="panel-body">
                             <p>With this option you can configure E.F.A. to relay outgoing messages for your local mail-server or clients</p>
                             <div class="table-responsive">
@@ -129,18 +140,18 @@ include '../inc/sidebar.php';
                                 </table>
                                 <button type="submit" class="btn btn-default">Add row</button>
                             </div>
-                            <!-- /.table-responsive -->
+                             /.table-responsive 
                         </div>
-                        <!-- /.panel-body -->
+                         /.panel-body 
                     </div>
-                    <!-- /.panel -->
+                     /.panel 
                 </div>
-                <!-- /.col-lg-6 -->
+                 /.col-lg-6 
                 </form>
             </div>
-            <!-- /Outbound mail relay.row -->
+             /Outbound mail relay.row -->
 
-            <!-- adminemail.row -->
+<!--             adminemail.row 
             <div class="row">
                 <form role="form-adminemail">
                 <div class="col-lg-12">
@@ -148,7 +159,7 @@ include '../inc/sidebar.php';
                         <div class="panel-heading">
                             Admin Email
                         </div>
-                        <!-- /.panel-heading -->
+                         /.panel-heading 
                         <div class="panel-body">
                             <p>The admin emmail address is used for various system alerts and notifications</p>
                             <div class="table-responsive">
@@ -163,16 +174,16 @@ include '../inc/sidebar.php';
                                 </table>
                                 <button type="submit" class="btn btn-default">Save</button>
                             </div>
-                            <!-- /.table-responsive -->
+                             /.table-responsive 
                         </div>
-                        <!-- /.panel-body -->
+                         /.panel-body 
                     </div>
-                    <!-- /.panel -->
+                     /.panel 
                 </div>
-                <!-- /.col-lg-6 -->
+                 /.col-lg-6 
                 </form>
             </div>
-            <!-- /admin email.row -->
+             /admin email.row -->
 
         </div>
         <!-- /#page-wrapper -->
