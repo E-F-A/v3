fa8bd998946454873c38f69bc86d6f32b5a3b576
shawniverson <shawniverson@gmail.com>
2015-12-06 10:21:00 -0500

    2 Fixes
    Issue #179 https default
    Remove dnsmasq in ks.cfg

--- build/build.bash
+++ build/build.bash
@@ -555,6 +555,12 @@ func_apache () {
 
     # Issue #139 SSLv3 POODLE Vulnerability
     sed -i "/^SSLProtocol/ c\SSLProtocol all -SSLv2 -SSLv3" /etc/httpd/conf.d/ssl.conf
+    
+    # Issue #179 default https
+    sed -i '/^#Listen 443/ c\Listen 443' /etc/httpd/conf.d/ssl.conf
+    echo -e "RewriteEngine On" > /etc/httpd/conf.d/redirectssl.conf
+    echo -e "RewriteCond %{HTTPS} !=on" >> /etc/httpd/conf.d/redirectssl.conf
+    echo -e "RewriteRule ^/?(.*) https://%{SERVER_NAME}/\$1 [R,L]" >> /etc/httpd/conf.d/redirectssl.conf
 
     # Secure PHP (this might break some stuff so need to test carefully)
     sed -i '/disable_functions =/ c\disable_functions = apache_child_terminate,apache_setenv,define_syslog_variables,escapeshellcmd,eval,fp,fput,ftp_connect,ftp_exec,ftp_get,ftp_login,ftp_nb_fput,ftp_put,ftp_raw,ftp_rawlist,highlight_file,ini_alter,ini_get_all,ini_restore,inject_code,openlog,phpAds_remoteInfo,phpAds_XmlRpc,phpAds_xmlrpcDecode,phpAds_xmlrpcEncode,posix_getpwuid,posix_kill,posix_mkfifo,posix_setpgid,posix_setsid,posix_setuid,posix_setuid,posix_uname,proc_close,proc_get_status,proc_nice,proc_open,proc_terminate,syslog,system,xmlrpc_entity_decode,curl_multi_exec' /etc/php.ini
