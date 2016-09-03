66aba75bab9a14edf9defef424d019c53a3ccce9
shawniverson <shawniverson@gmail.com>
2016-09-03 12:12:37 -0400

    Version updates

--- build/prepare-build-without-ks.bash
+++ build/prepare-build-without-ks.bash
@@ -281,7 +281,7 @@ alsa-lib
 #----------------------------------------------------------------#
 mkdir /var/log/EFA
 mkdir /usr/src/EFA
-/usr/bin/wget -q -O /usr/src/EFA/build.bash -o /var/log/EFA/wget.log https://raw.githubusercontent.com/E-F-A/v3/3.0.1.2-beta/build/build.bash --no-check-certificate
+/usr/bin/wget -q -O /usr/src/EFA/build.bash -o /var/log/EFA/wget.log https://raw.githubusercontent.com/E-F-A/v3/3.0.1.2/build/build.bash --no-check-certificate
 chmod 700 /usr/src/EFA/build.bash
 #----------------------------------------------------------------#
 
