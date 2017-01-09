44096a27d972e37f2966331901724770e77ddc18
darky83 <darky83@troublenow.org>
2017-01-09 19:53:11 +0100

    Allow https by default

--- build/prepare-build-without-ks.bash
+++ build/prepare-build-without-ks.bash
@@ -108,6 +108,7 @@ cat > /etc/sysconfig/iptables << 'EOF'
 -A INPUT -p icmp -j ACCEPT
 -A INPUT -i lo -j ACCEPT
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+-A INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 25 -j ACCEPT
 -A INPUT -m state --state NEW -m tcp -p tcp --dport 10000 -j ACCEPT
