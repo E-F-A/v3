f7547c3ed967858f167993bcdbbc10f258e4e3a3
darky83 <arno@troublenow.org>
2017-03-04 18:07:32 +0100

    Add mod_evasive and remove server signatures

--- build/build.bash
+++ build/build.bash
@@ -554,10 +554,16 @@ func_apache () {
     sed -i '/disable_functions =/ c\disable_functions = apache_child_terminate,apache_setenv,define_syslog_variables,escapeshellcmd,eval,fp,fput,ftp_connect,ftp_exec,ftp_get,ftp_login,ftp_nb_fput,ftp_put,ftp_raw,ftp_rawlist,highlight_file,ini_alter,ini_get_all,ini_restore,inject_code,openlog,phpAds_remoteInfo,phpAds_XmlRpc,phpAds_xmlrpcDecode,phpAds_xmlrpcEncode,posix_getpwuid,posix_kill,posix_mkfifo,posix_setpgid,posix_setsid,posix_setuid,posix_setuid,posix_uname,proc_close,proc_get_status,proc_nice,proc_open,proc_terminate,syslog,system,xmlrpc_entity_decode,curl_multi_exec' /etc/php.ini
 
     # Add mod_security
-    yum -y install mod_security mod_security_crs mod_security_crs-extras
+    yum -y install mod_security mod_security_crs mod_security_crs-extras mod_evasive
+      # Allow access on IP for users that don't use FQDN's
+      sed -i 's.</IfModule>.\n    SecRuleRemoveById 960017\n\n&.' /etc/httpd/conf.d/mod_security.conf
 
-    # Allow access on IP for users that don't use FQDN's
-    sed -i 's.</IfModule>.\n    SecRuleRemoveById 960017\n\n&.' /etc/httpd/conf.d/mod_security.conf
+    # Remove Server Signatures
+    sed -i "/^ServerSignature/ c\ServerSignature Off" /etc/httpd/conf/httpd.conf
+    sed -i "/^ServerTokens/ c\ServerTokens Prod" /etc/httpd/conf/httpd.conf
+
+    # Remove indexing
+    sed -i "/^    Options Indexes FollowSymLinks/ c\    Options -Indexes FollowSymLinks" /etc/httpd/conf/httpd.conf
 
     # set X-XSS-Protection header
     sed -i 's.</VirtualHost>.Header set X-XSS-Protection "1; mode=block"\n\n&.' /etc/httpd/conf.d/ssl.conf
